5473d99c275d86b6b70faeff3f50047426746aed
roseokpe <roseokpe@gmail.com>
2022-11-15 23:02:50 -0800

    add user mvc

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
 
     devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable, :confirmable
 
-    has_many :group, dependent: :destroy foreigh key: "user_id"
-    has_many :entity, dependent: :destroy foreigh key: "user_id"
+    has_many :group, dependent: :destroy
+    has_many :entity, dependent: :destroy
 
     validates :name, presence: true
     validates :email, presence: true
