12f9a04d1163eedda59d20e55ab54861b856ba01
roseokpe <roseokpe@gmail.com>
2022-11-15 23:01:02 -0800

    add group mvc

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,9 +1,7 @@
 class Group < ActiveRecord::Base
 
-  has_many :entity, dependent: :destroy foreigh key: "group_id"
-  has_many :user, dependent: :destroy foreigh key: "group_id"
+  has_many :entity, dependent: :destroy foreign key: "group_id"
+  has_many :user, dependent: :destroy foreign key: "group_id"
 
-    validates :name, presence: true
-
-
+  validates :name, presence: true
 end
