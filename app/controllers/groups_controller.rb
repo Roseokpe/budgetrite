1ffbdf6c6b58e34778442c89aaa2392fc1ea00d6
roseokpe <roseokpe@gmail.com>
2022-11-19 19:56:44 -0800

    update controllers

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,9 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: %i[ show edit update destroy ]
-
-  def index
-    @groups = Group.all
-  end
+  def index; end
 
   def show
     @group = Group.find(params[:id])
@@ -14,15 +10,14 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-  def edit;end
-
   def create
     @user = current_user
     @group = @user.groups.new(group_params)
+    @group.user_id = @user.id
 
     respond_to do |format|
       if @group.save
-        format.html { redirect_to group_url(@group), notice: "Group was successfully created." }
+        format.html { redirect_to group_url(@group), notice: 'Group was successfully created.' }
         format.json { render :show, status: :created, location: @group }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -34,7 +29,7 @@ class GroupsController < ApplicationController
   def update
     respond_to do |format|
       if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: "Group was successfully updated." }
+        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
         format.json { render :show, status: :ok, location: @group }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -47,17 +42,14 @@ class GroupsController < ApplicationController
     @group.destroy
 
     respond_to do |format|
-      format.html { redirect_to groups_url, notice: "Group was successfully destroyed." }
+      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    def set_group
-      @group = Group.find(params[:id])
-    end
 
-    def group_params
-      params.require(:group).permit(:name, :icon, :user_id)
-    end
+  def group_params
+    params.require(:group).permit(:name, :icon, :user_id)
+  end
 end
